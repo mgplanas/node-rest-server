2d1afdd138057b4ee13ec5830502190af091b9f2
mgplanas <mgplanas@gmail.com>
2019-02-14 12:24:17 -0300

    FIX MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,6 @@ if (process.env.NODE_ENV === 'DEV') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URI;
-    // urlDB = 'mongodb://mgplanas:Pepito123@planas-m001-shard-00-00-kku06.mongodb.net:27017,planas-m001-shard-00-01-kku06.mongodb.net:27017,planas-m001-shard-00-02-kku06.mongodb.net:27017/cafe?ssl=true&replicaSet=planas-m001-shard-0&authSource=admin&retryWrites=true';
 }
 
 process.env.URL_DB = urlDB;
