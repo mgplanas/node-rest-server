b155bd67b315c13dc81086b9092025c79cd4dd79
mgplanas <mgplanas@gmail.com>
2019-02-14 10:14:44 -0300

    MONGO_URI env variable

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'DEV') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     // urlDB = process.env.MONGO_URI;
-    urlDB = 'mongodb://planas-m001-shard-00-00-kku06.mongodb.net:27017/cafe?ssl=true';
+    urlDB = 'mongodb://mgplanas:Pepito123@planas-m001-shard-00-00-kku06.mongodb.net:27017/cafe?ssl=true';
     // urlDB = 'mongodb://mgplanas:Pepito123@planas-m001-shard-00-00-kku06.mongodb.net:27017/cafe?ssl=true';
 }
 
